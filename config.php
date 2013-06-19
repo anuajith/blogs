3b6a8b670f020f5655104e99c7545c311aa645a0
anuajith <anuugnair@gmail.com>
2013-06-19 18:09:21 +0545

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ *  Define Constants
+ */
+define("PAYPAL_PRO_ENV", "sandbox");//// 'sandbox' or 'beta-sandbox' or 'live'
+define("PAYPAL_PRO_API_UNAME", "some.gmail.com");
+define("PAYPAL_PRO_API_PWD", "1371618416");
+define("PAYPAL_PRO_API_SIG", "A-VtfyEXrNsmdCoafffff45sgGtIAMSVZNS0SdzIZFDn6UL8MTq88lgG");
+define("PAYPAL_PRO_API_URL", "https://api-3t.paypal.com/nvp");
+define("PAYPAL_PRO_API_VER", "76.0");
+define("PAYPAL_PAY_CURRENCY", "USD");
+define("PAYPAL_PAY_TYPE", "Sale");
+
+
+?>
